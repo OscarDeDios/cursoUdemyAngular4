4bed359cecc3f83e540115592237248079c01de9
Oscar de Dios <oscar.dedios.rodriguez@gmail.com>
2017-06-09 15:38:14 +0200

    Navigation with events

--- app-project/src/app/app.component.ts
+++ app-project/src/app/app.component.ts
@@ -6,6 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  recipes = true;
+  shoppingList = false;
+
   title = 'app works!';
   name = '';
+
+  showRecipes() {
+    this.recipes = true;
+    this.shoppingList = false;
+  }
+
+  showShoppingList() {
+    this.recipes = false;
+    this.shoppingList = true;
+  }
 }
--- app-project/src/app/header/header.component.ts
+++ app-project/src/app/header/header.component.ts
@@ -1,9 +1,26 @@
-import { Component} from '@angular/core'
+import { Component, OnInit, EventEmitter, Output} from '@angular/core'
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+    @Output() onShowRecipesEvent = new EventEmitter();
+    @Output() onShowShoppingListEvent = new EventEmitter();
+
+    
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+  showRecipes() {
+      this.onShowRecipesEvent.emit();
+  }
+
+  showShoppingList() {
+      this.onShowShoppingListEvent.emit();
+  }
 
 }
